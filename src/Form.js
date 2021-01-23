97adaa001a2dc248398e9b29a6be4971ba178374
tomnovacek <43985919+tomnovacek@users.noreply.github.com>
2021-01-23 12:44:31 +0100

    form updates

--- src/Form.js
+++ src/Form.js
@@ -6,6 +6,20 @@ function MetaForm() {
     <Form>
       <FormGroup row>
         <Label sm={2} for="formGroupExampleInput">
+          URL
+        </Label>
+        <Col>
+          <Input
+            sm={10}
+            type="text"
+            class="form-control"
+            id="url"
+            placeholder="www.reqview.com"
+          />
+        </Col>
+      </FormGroup>
+      <FormGroup row>
+        <Label sm={2} for="formGroupExampleInput">
           Title
         </Label>
         <Col>
@@ -13,22 +27,36 @@ function MetaForm() {
             sm={10}
             type="text"
             class="form-control"
-            id="formGroupExampleInput"
+            id="title"
             placeholder="Click Reqview"
           />
         </Col>
       </FormGroup>
       <FormGroup row>
         <Label sm={2} for="formGroupExampleInput2">
-          Metadata
+          Description
         </Label>
         <Col>
           <Input
             sm={10}
             type="text"
             class="form-control"
-            id="formGroupExampleInput2"
+            id="description"
             placeholder="ReqView is the best. Do not hesitate and buy it."
+          />
+        </Col>
+      </FormGroup>
+      <FormGroup row>
+        <Label sm={2} for="formGroupExampleInput2">
+          Bold kw
+        </Label>
+        <Col>
+          <Input
+            sm={10}
+            type="text"
+            class="form-control"
+            id="bold"
+            placeholder="Requirements management"
           />
         </Col>
       </FormGroup>
