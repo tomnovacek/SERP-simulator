4aa9f5df63de6b39879b1864ba4fd1538fd25070
Tom <tom.novacek@icloud.com>
2021-02-04 21:23:26 +0100

    Character counter added
    For optimal length check.

--- src/Serp.js
+++ src/Serp.js
@@ -4,10 +4,10 @@ function Serp(props) {
   return (
     <div className="serp-preview">
       <div className="serp-url">
-        {props.url} <span className="serp-arrow"></span>
+        {props.url} <span className="serp-arrow"></span> <span className="ml-3 text-muted" style={{fontSize:"10px"}}>{props.url.length}</span>
       </div>
-      <div className="serp-title">{props.title}</div>
-      <div className="serp-description">{props.description}</div>
+      <div className="serp-title">{props.title}<span className="ml-3 text-muted" style={{fontSize:"10px"}}>{props.title.length}</span></div>
+      <div className="serp-description">{props.description}<span className="ml-3 text-muted" style={{fontSize:"10px"}}>{props.description.length}</span></div>
     </div>
   );
 }
