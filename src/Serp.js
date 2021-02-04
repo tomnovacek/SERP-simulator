305bf998ef332f37d705f0bbc157fd8a7405c8d3
Tom <tom.novacek@icloud.com>
2021-02-04 21:47:25 +0100

    Counter - improved styling

--- src/Serp.js
+++ src/Serp.js
@@ -4,10 +4,10 @@ function Serp(props) {
   return (
     <div className="serp-preview">
       <div className="serp-url">
-        {props.url} <span className="serp-arrow"></span> <span className="ml-3 text-muted" style={{fontSize:"10px"}}>{props.url.length}</span>
+        {props.url} <span className="serp-arrow"></span> <span className="serp-counter" >{props.url.length}</span>
       </div>
-      <div className="serp-title">{props.title}<span className="ml-3 text-muted" style={{fontSize:"10px"}}>{props.title.length}</span></div>
-      <div className="serp-description">{props.description}<span className="ml-3 text-muted" style={{fontSize:"10px"}}>{props.description.length}</span></div>
+      <div className="serp-title">{props.title}<span className="serp-counter">{props.title.length}</span></div>
+      <div className="serp-description">{props.description}<span className="serp-counter">{props.description.length}</span></div>
     </div>
   );
 }
